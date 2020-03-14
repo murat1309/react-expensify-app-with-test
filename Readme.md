3fab992d7a8a48072ab05f674729a540070eb5be
murat1309 <muratcan.gokyokus@hotmail.com>
2020-03-15 00:36:08 +0300

    setup production webpck build

--- Readme.md
+++ Readme.md
@@ -34,11 +34,46 @@ in package json => "test": "jest --config=jest.config.json"
 
 =====> yarn test -- --watch 
 
----------------------DEOPLOY---------------
+---------------------DEOPLOY --GİT--SSH--Webpack-----------------
+
+
 1.) git pc'de yoksa "https://git-scm.com/downloads" sitesinden indir konsolda "git --version" kontrolünü yap
 2.) git init
 3.) "git status" yaptın nelerin gidiceğini gördün ancak gitmesini istemediğin dosyaları için mesela node_modules .gitignore oluşturdun.
 4.) tekrar "git status" yap ve .gitignore klasöründe eklediğin dosyaların bu sefer görünmediğini görüceksin.
 5.) "git add ." ile herşeyi aldım dedin. git status ile kontrol ettin.
-6.) "git commit -m "Initial commit"
+6.) "git commit -m "Initial commit" (note => gşt log ile yapılan commit'leri görebilirsin.)
+
+-----------#Git Commands#--------------
+git init => Create a new git repo
+git status => View the changes to your project code
+git add . => Add files to staging area
+git commit => Create a new commit with files from staging area
+ git log => View recent commits
+
+---------------Setting SSH and Github-----------------
+1.) Create a new repository
+2.) proje dizinine gidip git bash başlat ve "ls -a ~/.ssh" komutu çalıştır. (zaten varsa listelenecektir.)
+3.) ssh-keygen -t rsa -b 4096 -C "muratcan.gokyokus@hotmail.com" (yukardakinde zaten görünüyorsa tekrar oluşturmana gerek yok)
+4.) olsada olmasada bu adımdan devam => eval $(ssh-agent -s) (Ssh-agent'ın çalıştığından emin oluyoruz. Bir pid nuaraı görünüyor.)
+5.) ssh-add ~/.ssh/id_rsa (Add your SSH private key to the ssh-agent.)
+6.) clip < ~/.ssh/id_rsa.pub (keyi kopyaladık 6.1' maddede yapıştırcaz)
+ 6.1) githuba geç => settings => SSH and GPG keys => New SSH key =>  burda yapıştırıyoruz.
+7.) artık githubdaki şu 2 maddeyi yapabiliriz. (Https değil ssh olanı seçtikten sonra 2 maddenin ilkini yapıştırıyorum aşağıdaki maddede)
+ 7.1) git remote add origin git@github.com:murat1309/react-expensify-app-with-test.git
+ 7.2) git push -u origin master (artık github'da kodlarını görebilirsin.)
  
+----------------Webpack-----
+1.)package.json ve webpack.config dosyalarında production için bazı yarlamalar yaptık (webpck -p --env production)
+
+----------------CSS----------
+1.) yarn add extract-text-webpack-plugin@3.0.0 
+    Bundle js içindeki css yükünden kurtulmak için kütüphane yüklüyoruz.
+    Böylece style'lar artık JS paketinde değil, ayrı bir CSS dosyası (styles.css) içine alınmıştır. 
+    Toplam style sayfası biriminiz büyükse, CSS paketi JS paketine paralel olarak yüklendiği için daha hızlı olacaktır.
+    
+2.) webpack.config.js'de ilgili düzenlemeler yapıldıktan sonra tekrar npm run build:prod çalıştırırsan artık css dosyaların styles.css altında çalışacaktır.     
+3.) index.html sayfasında sayfa ilk açıldığında css'lerin yüklenebilmesi için yeni oluşan styles.css dosyasını <link> olarak eklemen lazım yoksa css yüklenmez..   
+4.) npm run serve => ile çalıştır bakalım.
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,54 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-console.log(path.resolve(__dirname, 'public')); //C:\Users\murat\react-tutorial-New\r-06-08-saving-loading-count\public
+//console.log(path.resolve(__dirname, 'public')); //C:\Users\murat\react-tutorial-New\r-06-08-saving-loading-count\public
 
-module.exports = {
-    entry: './src/app.js', //tell webpack where it should start.
-    output: {
-        path: path.resolve(__dirname, 'public'), //where you wanna output webpack file (bundle.js)
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, //only when files meet this criteria (.js) will be run babel
-            exclude: /node_modules/ //we dont wanna run babel those libraries
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    //development ortamında gerekli çünkü bi hata olduğunda bunu konsolda webpack'in oluşturduğu bundle.js'de görüyordun.
-    //biz bu kodu ekleyerek gerçekten bizim yazdığımız kendi kodumuza karşılık gelen yeri görebiliriz.
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'), //where it can find out public files.
-        historyApiFallback: true                    //Similar to how configured live-server in package.json(live-server yerine geçicek gibi düşün.)
-    }
+module.exports = (env) => {
+    console.log('env',env); //package.json'da build:prod'da --env olarak verdiğin değer burdaki env'e karşılık gelecektir. Wooww!!.
+
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js', //tell webpack where it should start.
+        output: {
+            path: path.resolve(__dirname, 'public'), //where you wanna output webpack file (bundle.js)
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, //only when files meet this criteria (.js) will be run babel
+                exclude: /node_modules/ //we dont wanna run babel those libraries
+            },{
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        //development ortamında gerekli çünkü bi hata olduğunda bunu konsolda webpack'in oluşturduğu bundle.js'de görüyordun.
+        //biz bu kodu ekleyerek gerçekten bizim yazdığımız kendi kodumuza karşılık gelen yeri görebiliriz.
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'), //where     it can find out public files.
+            historyApiFallback: true                    //Similar to how configured live-server in package.json(live-server yerine geçicek gibi düşün.)
+        }
+    };
 };
